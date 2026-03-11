0ac80602ad23ef487b4bff0ecf6d904b1ba571c3
Cheng <g0927756978@gmail.com>
2026-03-11 15:46:32 +0800

    Update and rename Lab02_Q2.cpp to Lab02_Ex2.cpp

---
+++ Lab02/Lab02_Ex2.cpp
@@ -0,0 +1,10 @@
+void printPairs(const vector<int>& arr) {
+// TODO: Add counts for outer loop initialization
+for (int i = 0; i < arr.size(); i++) {
+// TODO: Add counts for inner loop
+for (int j = i + 1; j < arr.size(); j++) {
+// TODO: Add counts for printing operation
+cout << arr[i] << "," << arr[j] << endl;
+}
+}
+}
